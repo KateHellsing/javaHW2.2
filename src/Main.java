92bba5dd08820865dcf07d910c106392ac292c3a
KateHellsing <ekaterina.hellsing999@gmail.com>
2022-10-11 23:52:27 +0300

    bonus

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double userBalance = 100;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Top-up amount: ");
+        double income = scan.nextDouble();
+        int bonus;
+        if (income > 1000) {
+            bonus = (int) income / 100;
+        } else {
+            bonus = 0;
+        }
+
+        userBalance += income + bonus;
+
+        System.out.print("Bonus: ");
+        System.out.println(bonus);
+        System.out.print("Balance: ");
+        System.out.println(userBalance);
+    }
+
+}
